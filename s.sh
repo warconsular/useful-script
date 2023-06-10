2d2efeab61aa1114bc14902fd3b94ddb158120aa
warconsular <114513984+warconsular@users.noreply.github.com>
2023-06-10 13:40:05 +0300

    Add files via upload

---
+++ s.sh
@@ -0,0 +1,21 @@
+!/bin/bash
+
+echo "Установка пакета React..."
+sudo yarn add react
+
+echo "Установка пакета React-Dom..."
+sudo yarn add react-dom
+
+echo "Установка пакета TypeScript..."
+sudo yarn add typescript
+
+echo "Установка пакета EslintTypeScript"
+sudo yarn add @typescript-eslint/types
+
+echo "Установка пакета Eslint"
+sudo yarn add eslint
+
+echo "Установка пакета Babel"
+sudo yarn add @babel/parser
+
+echo "Установка зависимостей завершена!"
