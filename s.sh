965cd6ccf4b7486aa0ff795ec79874a0954258af
warconsular <114513984+warconsular@users.noreply.github.com>
2023-06-10 14:08:32 +0300

    Update s.sh
    Installing typescript is sufficient

--- s.sh
+++ s.sh
@@ -9,9 +9,6 @@ sudo yarn add react-dom
 echo "Установка пакета TypeScript..."
 sudo yarn add typescript
 
-echo "Установка пакета EslintTypeScript"
-sudo yarn add @typescript-eslint/types
-
 echo "Установка пакета Eslint"
 sudo yarn add eslint
 
